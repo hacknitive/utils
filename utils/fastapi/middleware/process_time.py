96f034ba961877b030d60400605e13f1e1196c2f
Reza Aghamohammadi <hacknitive@gmail.com>
2025-02-24 21:59:25 +0330

    20250224

--- utils/fastapi/middleware/process_time.py
+++ utils/fastapi/middleware/process_time.py
@@ -13,5 +13,5 @@ def prepare_process_time_header(
     ):
         start_time = time()
         response = await call_next(request)
-        response.headers["X-Process-Time"] = str(round(time() - start_time), 6)
+        response.headers["X-Process-Time"] = str(round(time() - start_time, 4))
         return response
