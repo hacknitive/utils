c108ca1d2ce9b87eda1062e1d5b04e9865aa0a3e
Reza Aghamohammadi <hacknitive@gmail.com>
2025-08-09 20:13:01 +0330

    20250809

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r", encoding="utf-8") as handler:
 # ===========================================================
 setup(
     name="utils",
-    version="v1.0.0",
+    version="v1.0.1",
     packages=find_packages(include=['utils', 'utils.*']),
     install_requires=[],
     author="Reza 'Sam' Aghamohammadi (Hacknitive)",
